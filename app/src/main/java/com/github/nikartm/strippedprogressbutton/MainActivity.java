acf666da63e58073ba5e7af5c864d4a6b1b90366
Ivan V <jvissun@gmail.com>
2018-03-29 12:21:58 +0300

    Added buttons to tests

--- app/src/main/java/com/github/nikartm/strippedprogressbutton/MainActivity.java
+++ app/src/main/java/com/github/nikartm/strippedprogressbutton/MainActivity.java
@@ -2,12 +2,18 @@ package com.github.nikartm.strippedprogressbutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnStart;
+    private Button btnStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStart = findViewById(R.id.btn_start);
+        btnStop = findViewById(R.id.btn_stop);
     }
 }
